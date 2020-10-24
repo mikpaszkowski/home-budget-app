0c97b65d0bccd6952ba2ae7c06eca1744f9529b4
Miki <paszkowski.mikolaj98@gmail.com>
2020-10-24 20:23:28 +0200

    DOM elements manipulation/animations

--- app.js
+++ app.js
@@ -2,16 +2,7 @@
 
 var serviceController = (function () {
 
-    var Expense = function(id, amount, category, date, time, description){
-        
-        this.id = id;
-        this.amount = amount;
-        this.category = category;
-        this.date = date;
-        this.time = time;
-        this.description = description;
-    }
-    var Income = function(id, amount, category, date, time, description){
+    var Expense = function (id, amount, category, date, time, description) {
 
         this.id = id;
         this.amount = amount;
@@ -19,6 +10,49 @@ var serviceController = (function () {
         this.date = date;
         this.time = time;
         this.description = description;
+    }
+    var Income = function (id, amount, category, date, time, description) {
+
+        this.id = id;
+        this.amount = amount;
+        this.category = category;
+        this.date = date;
+        this.time = time;
+        this.description = description;
+    }
+
+    var data = {
+        allRecords: {
+            exp: [],
+            inc: []
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+        },
+    }
+
+    return {
+        addItem: function (type, amount, cat, date, time, des) {
+            var newRecord, id;
+
+            if (data.allRecords[type].length > 0) {
+                id = data.allRecords[type][data.allRecords[type].length - 1].id + 1;
+            } else {
+                id = 0;
+            }
+
+            if (type === 'inc') {
+                newRecord = new Income(id, amount, cat, date, time, des);
+            } else if (type === 'exp') {
+                newRecord = new Expense(id, amount, cat, date, time, des);
+            }
+            data.allRecords[type].push(newRecord);
+            return newRecord;
+        },
+        displayData: function () {
+            console.log(data.allRecords);
+        }
     }
 })();
 
@@ -36,6 +70,7 @@ var viewController = (function () {
         typeBtnExpense: '.expense-btn',
         typeBtnIncome: '.income-btn',
         closeIconBtn: '.close-icon',
+        recordContainerList: '.record-list',
     }
 
     var DOMStyleElements = {
@@ -50,13 +85,13 @@ var viewController = (function () {
         getInput: function () {
             var typeButton = document.querySelector('.income-btn');
             if (typeButton.classList.contains('active-record-type-green')) {
-                typeButton = '+';
+                typeButton = 'inc';
             } else {
-                typeButton = '-';
+                typeButton = 'exp';
             }
 
             return {
-                typeOfInputAmount: typeButton,
+                type: typeButton,
                 category: document.querySelector(DOMelements.inputCategory).value,
                 amount: document.querySelector(DOMelements.inputAmount).value,
                 date: document.querySelector(DOMelements.inputDate).value,
@@ -64,6 +99,24 @@ var viewController = (function () {
                 description: document.querySelector(DOMelements.inputDescription).value
             };
         },
+        addListRecord: function (obj, type) {
+            var html, newHtml, element;
+
+            if (type === 'inc') {
+                html = '<div class="record-income-item" id="income-record-%id%"><div class="head-of-record"><input type="checkbox" name="select"><div class="record-name">%des%</div></div><div class="amount-container"><div class="amount-value">+%amount%</div><div class="delete-icon"><i class="ion-ios-close-outline hidden"></i></div></div></div>';
+            } else if (type === 'exp') {
+                html = '<div class="record-expense-item" id="expense-record-%id%"><div class="head-of-record"><input type = "checkbox" name = "select"><div class="record-name">%des%</div></div><div class="amount-container"><div class="amount-value">-%amount%</div><div class="delete-icon"><i class="ion-ios-close-outline hidden"></i></div></div></div>';
+            }
+
+            newHtml = html.replace('%id%', obj.id);
+            newHtml = newHtml.replace('%des%', obj.description);
+            newHtml = newHtml.replace('%amount%', obj.amount);
+
+            document.querySelector(DOMelements.recordContainerList).insertAdjacentHTML('beforeend', newHtml);
+
+        },
+
+
         getDOMelements: function () {
             return DOMelements;
         },
@@ -119,17 +172,26 @@ var appController = (function () {
 
     var addItemController = function () {
 
+        var input, newRecord;
+
         //getting input data from front
-        var input = viewController.getInput();
+        input = viewController.getInput();
+        console.log(input);
 
         // 2. Add the item to the budget controller
+        newRecord = serviceController.addItem(input.type, input.amount,
+            input.category, input.date, input.time, input.description);
+        serviceController.displayData();
+
         // 3. Add the item to the UI
+        viewController.addListRecord(newRecord, input.type);
+
         // 4. Calculate the budget
         // 5. Display the budget on the UI
     }
 
-    return{
-        init: function() {
+    return {
+        init: function () {
             console.log('Application has started.');
             setUpEventListeners();
         }
